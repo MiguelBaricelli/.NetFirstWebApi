725f0b2c36231d5ae9cdccaa724ae956ed50eaca
Miguel Augusto Baricelli <baricellimiguel@gmail.com>
2025-04-10 15:49:53 -0300

    Atualiza nova implementação da Key JWT na API WEB

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/v1/auth")]
+    public class AuthController : Controller
+    {
+        [HttpPost]
+        public IActionResult Auth(string username, string password)
+        {
+            if (username == "miguel" && password == "123456")
+            {
+                var token = TokenServices.GenerateToken(new Model.Employee());
+                return Ok(token);
+            }
+
+            return BadRequest("username of password invalid");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WebApplication1.Infraestutura;
 using WebApplication1.Model;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,9 +12,62 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+    {
+        new OpenApiSecurityScheme
+        {
+        Reference = new OpenApiReference
+            {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+            },
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header,
+
+        },
+        new List<string>()
+        }
+    });
+
+
+});
 
 builder.Services.AddTransient<IEmployeerRepository, EmployeeRepository>();
+
+
+var key = Encoding.ASCII.GetBytes(WebApplication1.Key.Secret);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+    };
+});
 
 var app = builder.Build();
 
