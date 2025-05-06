f9651077f78a0a41ab3822bdfbdfebb21eca5119
Miguel Augusto Baricelli <142127570+MiguelBaricelli@users.noreply.github.com>
2025-05-06 15:01:51 -0300

    Update Key.cs

--- Key.cs
+++ Key.cs
@@ -2,6 +2,7 @@
 {
     public class Key
     {
-        public static string Secret = "K8gZrM9xT2vBnL5qWp7HdJe3Fy6RuA1k";
+     
+        public static string Secret = "----------------------------------";
     }
 }
